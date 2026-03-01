afa3d3de1fd3737815927ab427b165d718a78201
Gogul11 <gogul0811@gmail.com>
2026-03-01 20:19:48 +0530

    Hello

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,19 +1,40 @@
 import Link from 'next/link';
 import React from 'react';
-
+import Image from 'next/image';
 const Menubar = () => {
     return (
-        <div className='w-full h-8 fixed inset-0 z-50
+        <div className='w-full h-14 fixed inset-0 z-50
                         bg-one border-b border-two py-0.5 px-2
-                        flex flex-row gap-4 items-center'>
-            <p>Itrix 26 - logo</p>
-            <p className='text-sm text-ten cursor-pointer'>File</p>
-            <p className='text-sm text-ten cursor-pointer'>Edit</p>
-            <p className='text-sm text-ten cursor-pointer'>Help</p>
-            <Link
-                href='https://istaceg.in/'
-                target='__blank' 
-                className='text-sm text-ten cursor-pointer'>Ista</Link>
+                        flex flex-row gap-4 items-center justify-between'>
+            <div className='flex gap-4 items-center'>
+                
+                 <Image
+                    src="/itrix'26-logo.png"
+                    width={100}
+                    height={100}
+                    alt='Itrix-Logo'
+                />
+                <p className='text-sm text-ten cursor-pointer'>File</p>
+                <p className='text-sm text-ten cursor-pointer'>Edit</p>
+                <p className='text-sm text-ten cursor-pointer'>Selection</p>
+                <p className='text-sm text-ten cursor-pointer'>View</p>
+                <p className='text-sm text-ten cursor-pointer'>Help</p>
+
+            </div>
+            <div>
+
+                <Link
+                    href='https://istaceg.in/'
+                    target='__blank' 
+                >
+                    <Image
+                        src='/ista-logo.png'
+                        alt='Ista-Logo'
+                        width={50}
+                        height={50}
+                    />
+                </Link>
+            </div>
         </div>
     );
 }
